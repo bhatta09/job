27bddc82d01bac2d959673214d6198df715745bb
bhatta09 <sandesh@academiacollege.edu.np>
2024-01-04 07:14:03 +0545

    first commit

---
+++ src/Components/JobDiv/Jobs.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {BiTimeFive} from "react-icons/bi"
+
+const Jobs = () => {
+  return (
+    <div>
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px]
+        hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+
+          <span className='flex justify-between items-center gap-4'>
+          <h1 className='text-[16px] font-semibold text-textColor
+          group-hover:text-white'>Web Devloper</h1>
+         <span className='flex items-center text-[#ccc] gap-1'>
+          <BiTimeFive/>Now
+         </span>
+          </span>
+        <h6 className='text-[#ccc]'>Canada</h6>
+
+        <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px]
+        group-hover:text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, alias.
+        </p>
+
+        <div className='company flex items-center gap-2'>
+      <img src="" alt="" />
+
+    </div>
+      </div>
+    </div>
+    </div>
+    
+  );
+};
+
+export default Jobs;
